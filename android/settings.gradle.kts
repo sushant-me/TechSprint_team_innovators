892f4fcf605169100de7189e898041c294696987
Sushant Poudel <sushant.poudel2028@gmail.com>
2026-01-18 23:06:34 +0545

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,17 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    // 1. dynamic Flutter SDK Path Resolution
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        val path = properties.getProperty("flutter.sdk")
+        require(path != null) { "flutter.sdk not set in local.properties" }
+        path
+    }
 
+    // 2. Include the Flutter Gradle Tooling
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
@@ -18,9 +22,14 @@ pluginManagement {
 }
 
 plugins {
+    // This plugin handles loading Flutter plugins from pubspec.yaml
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+    // Android Gradle Plugin (AGP) - Version 8.3.2 is stable and supports Java 17
+    id("com.android.application") version "8.3.2" apply false
+    
+    // Kotlin Android Plugin - Version 1.9.24 is highly compatible with current Flutter
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 include(":app")
